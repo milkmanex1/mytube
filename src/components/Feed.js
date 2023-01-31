933527276a197996adb09aa12ba0ac445f0a753b
Gabriel <kokgabriel@gmail.com>
2023-01-31 16:02:43 +0800

    init

---
+++ src/components/Feed.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+const Feed = () => {
+  return (
+    <Stack sx={{ flexDirection: { sm: "column", md: "row" } }}>
+      <Box sx={{}}></Box>
+    </Stack>
+  );
+};
+
+export default Feed;
